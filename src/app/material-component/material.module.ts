abbc032dcaa9acdd91e01c8688418709892d399a
sahannt98 <sahantm0@gmail.com>
2023-10-18 18:48:20 +0530

    order page started

--- src/app/material-component/material.module.ts
+++ src/app/material-component/material.module.ts
@@ -13,6 +13,7 @@ import { ManageCategoryComponent } from './manage-category/manage-category.compo
 import { CategoryComponent } from './dialog/category/category.component';
 import { ManageProductComponent } from './manage-product/manage-product.component';
 import { ProductComponent } from './dialog/product/product.component';
+import { ManageOrderComponent } from './manage-order/manage-order.component';
 
 @NgModule({
   imports: [
@@ -31,7 +32,8 @@ import { ProductComponent } from './dialog/product/product.component';
     ManageCategoryComponent,
         CategoryComponent,
         ManageProductComponent,
-        ProductComponent
+        ProductComponent,
+        ManageOrderComponent
   ]
 })
 export class MaterialComponentsModule {}
---
+++ src/app/services/bill.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BillService {
+
+  url = 'http://localhost:8080/api/v1/';
+
+  constructor(
+    private httpClient:HttpClient
+  ) { }
+
+    generateReport(data:any){
+      return this.httpClient.post(this.url+"/bill/generatedReport",data,{
+        headers:new HttpHeaders().set('Content-Type',"application/json")
+      })
+    }
+
+    getPdf(data:any):Observable<Blob>{
+      return this.httpClient.post(this.url+"/bill/getPdf",data,{responseType:'blob'});
+    }
+
+    getBills(){
+      return this.httpClient.get(this.url+"/bill/getBills");
+    }
+
+
+}
